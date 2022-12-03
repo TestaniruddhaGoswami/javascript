b92e59bfc1e23190a64f3c5649a70fafecfbb278
Aniruddha Goswami <aniruddhagoswami777@gmail.com>
2022-12-03 11:52:08 +0530

    update:text

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-//input
-var username= prompt("give me your user name")
-//processing
-var welcomeMessage = "this script works  " + username;
-//output
-alert(welcomeMessage);
+var btnTranslate = document.querySelector( "#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector( "#output");
+
+
+
+
+console.log(txtInput);
+
+function clickHandler()
+{
+    console.log(" clicked !");
+    console.log("input", txtInput.value)
+};
+btnTranslate.addEventListener("click", clickHandler)
